626eec66a729112e7fed6d1a8c696cb3ab7caa72
Neno Arisma <nenoarisma@gmail.com>
2021-08-03 22:21:53 +0700

    Learn middleware

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const expressLayouts = require('express-ejs-layouts')
+const morgan = require('morgan')
 
 // use ejs as template engine and use express-ejs-layouts for layouting
 app.set('view engine', 'ejs')
+
+// Third-Party Middleware
 app.use(expressLayouts)
+app.use(morgan('dev'))
+
+// Build-In Middleware
+app.use(express.static('public'))
+
+// Application middleware
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  next()
+})
 
 app.get('/', (req, res) => {
   const barang = [{
@@ -32,19 +45,21 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
-  res.status(200).render('contact', { 
-    layout: 'layouts/main-layout', 
-    title: 'Contact Page' })
+  res.status(200).render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'Contact Page'
+  })
 })
 
-app.get('/product/:id', (req,res) => {
+app.get('/product/:id', (req, res) => {
   res.send(`ID produk anda adalah: ${req.params.id} <br> dengan kategori ${req.query.category}`)
 })
 
-app.use('/', (req, res) => {
-  res.status(404).render('error', { 
-    layout: 'layouts/main-layout', 
-    title: 'Upppss' })
+app.use((req, res) => {
+  res.status(404).render('error', {
+    layout: 'layouts/main-layout',
+    title: 'Upppss'
+  })
 })
 
 app.listen(port, () => {
